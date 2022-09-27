5f5c149211e2bb6c354f75b0325f87a9f3d55c44
Salou7a <114488656+Salou7a@users.noreply.github.com>
2022-09-27 15:05:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const { Client, Intents, Collection } = require("discord.js");
+const db = require("pro.db")
+const Discord = require('discord.js');
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
 
+
+client.login(process.env.token)
