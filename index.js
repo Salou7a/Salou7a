d93fc4d24e933418b91e47fb782c81067705af09
Salou7a <114488656+Salou7a@users.noreply.github.com>
2022-09-27 15:12:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Discord , Client, Intents, Collection } = require("discord.js");
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+  intents : 32767
 });
 
 
